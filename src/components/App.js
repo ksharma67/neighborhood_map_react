b69122f7019828c2f602e9d799bdc7f6bc1c0911
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2018-09-22 10:17:17 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,254 +1,215 @@
-import React, {Component} from 'react';
-import LocationList from './LocationList';
+import React, { Component } from "react";
+import LocationList from "./LocationList";
 
 class App extends Component {
-    /**
-     * Constructor
-     */
-    constructor(props) {
-        super(props);
-        this.state = {
-            'alllocations': [
-                {
-                    'name': "Bhaskar Vidhya Ashram",
-                    'type': "Private School",
-                    'latitude': 26.9053803,
-                    'longitude': 75.7259351,
-                    'streetAddress': "Lalarpura Road, Gandhi Path"
-                },
-                {
-                    'name': "Hotel Chhavi Holidays",
-                    'type': "Hotel",
-                    'latitude': 26.9055311,
-                    'longitude': 75.728137,
-                    'streetAddress': "Plot No. 11/12, Vivek Vihar"
-                },
-                {
-                    'name': "Handi",
-                    'type': "Restaurant",
-                    'latitude': 26.906990,
-                    'longitude': 75.742848,
-                    'streetAddress': "18, Gautam Marg, Vaishali Nagar"
-                },
-                {
-                    'name': "INOX - Amrapali",
-                    'type': "Movie Theater",
-                    'latitude': 26.912631,
-                    'longitude': 75.743389,
-                    'streetAddress': "C-1, Vaibhav Complex"
-                },
-                {
-                    'name': "Blue Dart",
-                    'type': "Courier Service",
-                    'latitude': 26.911103,
-                    'longitude': 75.738878,
-                    'streetAddress': "Vaishali Tower, Vaishali Nagar"
-                },
-                {
-                    'name': "Hotel Seven Seas",
-                    'type': "3-Star Hotel",
-                    'latitude': 26.906069,
-                    'longitude': 75.739583,
-                    'streetAddress': "A-6, Nemi Nagar, Gandhi Path"
-                },
-                {
-                    'name': "Global Heart & General Hospital",
-                    'type': "Hospital",
-                    'latitude': 26.905506,
-                    'longitude': 75.738762,
-                    'streetAddress': "C1/27, Opposite Bharat Apartment"
-                },
-                {
-                    'name': "Shri Swaminarayan Mandir",
-                    'type': "Hindu Temple",
-                    'latitude': 26.902167,
-                    'longitude': 75.740999,
-                    'streetAddress': "Sector 9, Chitrakoot"
-                },
-                {
-                    'name': "Pratap Marriage Garden",
-                    'type': "Banquet Hall",
-                    'latitude': 26.906464,
-                    'longitude': 75.732889,
-                    'streetAddress': "Arpit Nagar, B Block"
-                },
-                {
-                    'name': "ICICI Bank",
-                    'type': "Bank",
-                    'latitude': 26.913179,
-                    'longitude': 75.743447,
-                    'streetAddress': "Lalarpura Road, Gandhi Path"
-                }
-            ],
-            'map': '',
-            'infowindow': '',
-            'prevmarker': ''
-        };
+  /**
+   * Constructor
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      alllocations: require("./places.json"), // Get the locations from the JSON file
+      map: "",
+      infowindow: "",
+      prevmarker: ""
+    };
 
-        // retain object instance when used in the function
-        this.initMap = this.initMap.bind(this);
-        this.openInfoWindow = this.openInfoWindow.bind(this);
-        this.closeInfoWindow = this.closeInfoWindow.bind(this);
-    }
+    // retain object instance when used in the function
+    this.initMap = this.initMap.bind(this);
+    this.openInfoWindow = this.openInfoWindow.bind(this);
+    this.closeInfoWindow = this.closeInfoWindow.bind(this);
+  }
 
-    componentDidMount() {
-        // Connect the initMap() function within this class to the global window context,
-        // so Google Maps can invoke it
-        window.initMap = this.initMap;
-        // Asynchronously load the Google Maps script, passing in the callback reference
-        loadMapJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyCPi0o_tjNjKYYDe_6nYg82r0leI7kKlOE&callback=initMap')
-    }
+  componentDidMount() {
+    // Connect the initMap() function within this class to the global window context,
+    // so Google Maps can invoke it
+    window.initMap = this.initMap;
+    // Asynchronously load the Google Maps script, passing in the callback reference
+    loadMapJS(
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyCacmIGcWsolGnXLp71cBM_My9axyprocM&callback=initMap"
+    );
+  }
 
-    /**
-     * Initialise the map once the google map script is loaded
-     */
-    initMap() {
-        var self = this;
+  /**
+   * Initialise the map once the google map script is loaded
+   */
+  initMap() {
+    var self = this;
 
-        var mapview = document.getElementById('map');
-        mapview.style.height = window.innerHeight + "px";
-        var map = new window.google.maps.Map(mapview, {
-            center: {lat: 26.907502, lng: 75.737586},
-            zoom: 15,
-            mapTypeControl: false
-        });
+    var mapview = document.getElementById("map");
+    mapview.style.height = window.innerHeight + "px";
+    var map = new window.google.maps.Map(mapview, {
+      center: { lat: 3.885441, lng: 11.514515 },
+      zoom: 15,
+      mapTypeControl: false
+    });
 
-        var InfoWindow = new window.google.maps.InfoWindow({});
+    var InfoWindow = new window.google.maps.InfoWindow({});
 
-        window.google.maps.event.addListener(InfoWindow, 'closeclick', function () {
-            self.closeInfoWindow();
-        });
+    window.google.maps.event.addListener(InfoWindow, "closeclick", function() {
+      self.closeInfoWindow();
+    });
 
-        this.setState({
-            'map': map,
-            'infowindow': InfoWindow
-        });
+    this.setState({
+      map: map,
+      infowindow: InfoWindow
+    });
 
-        window.google.maps.event.addDomListener(window, "resize", function () {
-            var center = map.getCenter();
-            window.google.maps.event.trigger(map, "resize");
-            self.state.map.setCenter(center);
-        });
+    window.google.maps.event.addDomListener(window, "resize", function() {
+      var center = map.getCenter();
+      window.google.maps.event.trigger(map, "resize");
+      self.state.map.setCenter(center);
+    });
 
-        window.google.maps.event.addListener(map, 'click', function () {
-            self.closeInfoWindow();
-        });
+    window.google.maps.event.addListener(map, "click", function() {
+      self.closeInfoWindow();
+    });
 
-        var alllocations = [];
-        this.state.alllocations.forEach(function (location) {
-            var longname = location.name + ' - ' + location.type;
-            var marker = new window.google.maps.Marker({
-                position: new window.google.maps.LatLng(location.latitude, location.longitude),
-                animation: window.google.maps.Animation.DROP,
-                map: map
-            });
+    var alllocations = [];
+    this.state.alllocations.forEach(function(location) {
+      var longname = location.name + " - " + location.type;
+      var marker = new window.google.maps.Marker({
+        position: new window.google.maps.LatLng(
+          location.latitude,
+          location.longitude
+        ),
+        animation: window.google.maps.Animation.DROP,
+        map: map
+      });
 
-            marker.addListener('click', function () {
-                self.openInfoWindow(marker);
-            });
+      marker.addListener("click", function() {
+        self.openInfoWindow(marker);
+      });
 
-            location.longname = longname;
-            location.marker = marker;
-            location.display = true;
-            alllocations.push(location);
-        });
-        this.setState({
-            'alllocations': alllocations
-        });
-    }
+      location.longname = longname;
+      location.marker = marker;
+      location.display = true;
+      alllocations.push(location);
+    });
+    this.setState({
+      alllocations: alllocations
+    });
+  }
 
-    /**
-     * Open the infowindow for the marker
-     * @param {object} location marker
-     */
-    openInfoWindow(marker) {
-        this.closeInfoWindow();
-        this.state.infowindow.open(this.state.map, marker);
-        marker.setAnimation(window.google.maps.Animation.BOUNCE);
-        this.setState({
-            'prevmarker': marker
-        });
-        this.state.infowindow.setContent('Loading Data...');
-        this.state.map.setCenter(marker.getPosition());
-        this.state.map.panBy(0, -200);
-        this.getMarkerInfo(marker);
-    }
+  /**
+   * Open the infowindow for the marker
+   * @param {object} location marker
+   */
+  openInfoWindow(marker) {
+    this.closeInfoWindow();
+    this.state.infowindow.open(this.state.map, marker);
+    marker.setAnimation(window.google.maps.Animation.BOUNCE);
+    this.setState({
+      prevmarker: marker
+    });
+    this.state.infowindow.setContent("Loading Data...");
+    this.state.map.setCenter(marker.getPosition());
+    this.state.map.panBy(0, -200);
+    this.getMarkerInfo(marker);
+  }
 
-    /**
-     * Retrive the location data from the foursquare api for the marker and display it in the infowindow
-     * @param {object} location marker
-     */
-    getMarkerInfo(marker) {
-        var self = this;
-        var clientId = "TPIDDHBKB2QFBWEV2MPDOFGUSWXCXGAA5IVOWEMN5ASR3UJW";
-        var clientSecret = "4HB1ZZJBVXC3F0BREBPSGXYK0VZ5ALS4XRNJZSBP1JROG0DE";
-        var url = "https://api.foursquare.com/v2/venues/search?client_id=" + clientId + "&client_secret=" + clientSecret + "&v=20130815&ll=" + marker.getPosition().lat() + "," + marker.getPosition().lng() + "&limit=1";
-        fetch(url)
-            .then(
-                function (response) {
-                    if (response.status !== 200) {
-                        self.state.infowindow.setContent("Sorry data can't be loaded");
-                        return;
-                    }
+  /**
+   * Retrive the location data from the foursquare api
+   */
+  getMarkerInfo(marker) {
+    var self = this;
 
-                    // Examine the text in the response
-                    response.json().then(function (data) {
-                        var location_data = data.response.venues[0];
-                        var verified = '<b>Verified Location: </b>' + (location_data.verified ? 'Yes' : 'No') + '<br>';
-                        var checkinsCount = '<b>Number of CheckIn: </b>' + location_data.stats.checkinsCount + '<br>';
-                        var usersCount = '<b>Number of Users: </b>' + location_data.stats.usersCount + '<br>';
-                        var tipCount = '<b>Number of Tips: </b>' + location_data.stats.tipCount + '<br>';
-                        var readMore = '<a href="https://foursquare.com/v/'+ location_data.id +'" target="_blank">Read More on Foursquare Website</a>'
-                        self.state.infowindow.setContent(checkinsCount + usersCount + tipCount + verified + readMore);
-                    });
-                }
-            )
-            .catch(function (err) {
-                self.state.infowindow.setContent("Sorry data can't be loaded");
-            });
-    }
+    // Add the api keys for foursquare
+    var clientId = "Q5MK2XFDK3FVTDQLOQKSFTKS1CI1XEWZSO2TIPP5DU2PWICK";
+    var clientSecret = "MQ3CZLR5KY1F04FUAX5YWXOLYRRJSYFWCHZANZZ23M4WI05L";
 
-    /**
-     * Close the infowindow for the marker
-     * @param {object} location marker
-     */
-    closeInfoWindow() {
-        if (this.state.prevmarker) {
-            this.state.prevmarker.setAnimation(null);
+    // Build the api endpoint
+    var url =
+      "https://api.foursquare.com/v2/venues/search?client_id=" +
+      clientId +
+      "&client_secret=" +
+      clientSecret +
+      "&v=20130815&ll=" +
+      marker.getPosition().lat() +
+      "," +
+      marker.getPosition().lng() +
+      "&limit=1";
+    fetch(url)
+      .then(function(response) {
+        if (response.status !== 200) {
+          self.state.infowindow.setContent("Sorry data can't be loaded");
+          return;
         }
-        this.setState({
-            'prevmarker': ''
-        });
-        this.state.infowindow.close();
-    }
 
-    /**
-     * Render function of App
-     */
-    render() {
-        return (
-            <div>
-                <LocationList key="100" alllocations={this.state.alllocations} openInfoWindow={this.openInfoWindow}
-                              closeInfoWindow={this.closeInfoWindow}/>
-                <div id="map"></div>
-            </div>
-        );
+        // Get the text in the response
+        response.json().then(function(data) {
+          console.log(data);
+
+          var location_data = data.response.venues[0];
+          var place = `<h3>${location_data.name}</h3>`;
+          var street = `<p>${location_data.location.formattedAddress[0]}</p>`;
+          var contact = "";
+          if (location_data.contact.phone)
+            contact = `<p><small>${location_data.contact.phone}</small></p>`;
+          var checkinsCount =
+            "<b>Number of CheckIn: </b>" +
+            location_data.stats.checkinsCount +
+            "<br>";
+          var readMore =
+            '<a href="https://foursquare.com/v/' +
+            location_data.id +
+            '" target="_blank">Read More on <b>Foursquare Website</b></a>';
+          self.state.infowindow.setContent(
+            place + street + contact + checkinsCount + readMore
+          );
+        });
+      })
+      .catch(function(err) {
+        self.state.infowindow.setContent("Sorry data can't be loaded");
+      });
+  }
+
+  /**
+   * Close the info window previously opened
+   *
+   * @memberof App
+   */
+  closeInfoWindow() {
+    if (this.state.prevmarker) {
+      this.state.prevmarker.setAnimation(null);
     }
+    this.setState({
+      prevmarker: ""
+    });
+    this.state.infowindow.close();
+  }
+
+  /**
+   * Render for react
+   */
+  render() {
+    return (
+      <div>
+        <LocationList
+          key="100"
+          alllocations={this.state.alllocations}
+          openInfoWindow={this.openInfoWindow}
+          closeInfoWindow={this.closeInfoWindow}
+        />
+        <div id="map" />
+      </div>
+    );
+  }
 }
 
 export default App;
 
 /**
- * Load the google maps Asynchronously
- * @param {url} url of the google maps script
+ * Load the google maps
+ * @param {src} url of the google maps script
  */
 function loadMapJS(src) {
-    var ref = window.document.getElementsByTagName("script")[0];
-    var script = window.document.createElement("script");
-    script.src = src;
-    script.async = true;
-    script.onerror = function () {
-        document.write("Google Maps can't be loaded");
-    };
-    ref.parentNode.insertBefore(script, ref);
+  var ref = window.document.getElementsByTagName("script")[0];
+  var script = window.document.createElement("script");
+  script.src = src;
+  script.async = true;
+  script.onerror = function() {
+    document.write("Google Maps can't be loaded");
+  };
+  ref.parentNode.insertBefore(script, ref);
 }
--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,83 +1,81 @@
-/**
- * Created by manish on 6/1/18.
- */
-import React, {Component} from 'react';
-import LocationItem from './LocationItem';
+import React, { Component } from "react";
+import Place from "./Place";
 
 class LocationList extends Component {
-    /**
-     * Constructor
-     */
-    constructor(props) {
-        super(props);
-        this.state = {
-            'locations': '',
-            'query': '',
-            'suggestions': true,
-        };
+  /**
+   * Constructor
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      locations: "",
+      query: "",
+      suggestions: true
+    };
 
-        this.filterLocations = this.filterLocations.bind(this);
-        this.toggleSuggestions = this.toggleSuggestions.bind(this);
-    }
+    this.filterLocations = this.filterLocations.bind(this);
+  }
 
-    /**
-     * Filter Locations based on user query
-     */
-    filterLocations(event) {
-        this.props.closeInfoWindow();
-        const {value} = event.target;
-        var locations = [];
-        this.props.alllocations.forEach(function (location) {
-            if (location.longname.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-                location.marker.setVisible(true);
-                locations.push(location);
-            } else {
-                location.marker.setVisible(false);
-            }
-        });
+  /**
+   * Filter Locations based on user query
+   */
+  filterLocations(event) {
+    this.props.closeInfoWindow();
+    const { value } = event.target;
+    var locations = [];
+    this.props.alllocations.forEach(function(location) {
+      if (location.longname.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+        location.marker.setVisible(true);
+        locations.push(location);
+      } else {
+        location.marker.setVisible(false);
+      }
+    });
 
-        this.setState({
-            'locations': locations,
-            'query': value
-        });
-    }
+    this.setState({
+      locations: locations,
+      query: value
+    });
+  }
 
-    componentWillMount() {
-        this.setState({
-            'locations': this.props.alllocations
-        });
-    }
+  componentWillMount() {
+    this.setState({
+      locations: this.props.alllocations
+    });
+  }
 
-    /**
-     * Show and hide suggestions
-     */
-    toggleSuggestions() {
-        this.setState({
-            'suggestions': !this.state.suggestions
-        });
-    }
+  /**
+   * Render function of LocationList
+   */
+  render() {
+    var locationlist = this.state.locations.map(function(listItem, index) {
+      return (
+        <Place
+          key={index}
+          openInfoWindow={this.props.openInfoWindow.bind(this)}
+          data={listItem}
+        />
+      );
+    }, this);
 
-    /**
-     * Render function of LocationList
-     */
-    render() {
-        var locationlist = this.state.locations.map(function (listItem, index) {
-            return (
-                <LocationItem key={index} openInfoWindow={this.props.openInfoWindow.bind(this)} data={listItem}/>
-            );
-        }, this);
-
-        return (
-            <div className="search">
-                <input role="search" aria-labelledby="filter" id="search-field" className="search-field" type="text" placeholder="Filter"
-                       value={this.state.query} onChange={this.filterLocations}/>
-                <ul>
-                    {this.state.suggestions && locationlist}
-                </ul>
-                <button className="button" onClick={this.toggleSuggestions}>Show/Hide Suggestions</button>
-            </div>
-        );
-    }
+    return (
+      <div className="search-area">
+        <input
+          role="search"
+          aria-labelledby="filter"
+          id="search-field"
+          className="search-input"
+          type="text"
+          placeholder="Filter"
+          value={this.state.query}
+          onChange={this.filterLocations}
+        />
+        <ul className="location-list">
+          {this.state.suggestions && locationlist}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default LocationList;
